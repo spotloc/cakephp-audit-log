87ec4608b60978fa7e08435d037aeb00644a541a
Dlr <david.larouziere@gmail.com>
2020-11-27 08:57:30 +0100

    Take Authentication changes into account

--- src/Model/Table/CurrentUserTrait.php
+++ src/Model/Table/CurrentUserTrait.php
@@ -1,20 +1,24 @@
 <?php
+
 namespace AuditLog\Model\Table;
 
 use Cake\Http\ServerRequestFactory;
 
 trait CurrentUserTrait
 {
-  public function currentUser()
-  {
 
-    $request = ServerRequestFactory::fromGlobals();
-    $session = $request->getSession();
-    return [
-        'id' => $session->read('Auth.User.username'),
-        'ip' => $request->getEnv('REMOTE_ADDR'),
-        'url' => $request->getRequestTarget(),
-        'description' => $session->read('Auth.User.username'),
-    ];
-  }
+    public function currentUser()
+    {
+
+        $request = ServerRequestFactory::fromGlobals();
+        $session = $request->getSession();
+        $user = $session->read('Auth');
+        return [
+            'id'          => $user->username,
+            'ip'          => $request->getEnv('REMOTE_ADDR'),
+            'url'         => $request->getRequestTarget(),
+            'description' => $session->read('Auth.User.username'),
+        ];
+    }
+
 }
